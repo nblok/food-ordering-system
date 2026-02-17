0f6faab35b1882fcfa8048692834de3b4c6376dc
Nathan Blok <ndblok@gmail.com>
2026-02-17 07:39:15 -0800

    Added order domain events

---
+++ projects/order-service/src/order_service/order_domain/domain_core/event/order_event.py
@@ -0,0 +1,11 @@
+import datetime
+from common.common_domain.event.domain_event import DomainEvent
+
+from ..entity.order import Order
+
+
+class OrderEvent(DomainEvent[Order]):
+
+    def __init__(self, order: Order, created_at: datetime.datetime):
+        self.order = order
+        self.created_at = created_at
