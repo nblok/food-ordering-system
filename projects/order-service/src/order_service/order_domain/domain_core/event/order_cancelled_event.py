59122a7e3c0e57ce8a71d05863054f3a1ddcc375
Nathan Blok <ndblok@gmail.com>
2026-02-17 07:41:38 -0800

    refactored common code out of specific order domain events, and created a base class

--- projects/order-service/src/order_service/order_domain/domain_core/event/order_cancelled_event.py
+++ projects/order-service/src/order_service/order_domain/domain_core/event/order_cancelled_event.py
@@ -1,11 +1,5 @@
-import datetime
-from common.common_domain.event.domain_event import DomainEvent
-
-from ..entity.order import Order
+from .order_event import OrderEvent
 
 
-class OrderCancelledEvent(DomainEvent[Order]):
-
-    def __init__(self, order: Order, created_at: datetime.datetime):
-        self.order = order
-        self.created_at = created_at
+class OrderCancelledEvent(OrderEvent):
+    pass
