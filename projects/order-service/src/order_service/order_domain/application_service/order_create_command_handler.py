f0192749d6341d68992819c1e67fe25c87c52ed6
Nathan Blok <ndblok@gmail.com>
2026-02-19 15:25:23 -0800

    implemented having the order created event published outside of transaction.

--- projects/order-service/src/order_service/order_domain/application_service/order_create_command_handler.py
+++ projects/order-service/src/order_service/order_domain/application_service/order_create_command_handler.py
@@ -1,65 +1,31 @@
 import logging
-from uuid import UUID
 
 from order_service.order_domain.application_service.dto.create.create_order_command import CreateOrderCommand
 from order_service.order_domain.application_service.dto.create.create_order_response import CreateOrderResponse
 from order_service.order_domain.application_service.mapper.order_data_mapper import OrderDataMapper
-from order_service.order_domain.application_service.ports.output.repository.customer_repository import \
-    CustomerRepository
-from order_service.order_domain.application_service.ports.output.repository.order_repository import OrderRepository
-from order_service.order_domain.application_service.ports.output.repository.restaurant_repository import \
-    RestaurantRepository
-from order_service.order_domain.domain_core.exception.order_domain_exception import OrderDomainException
-from order_service.order_domain.order_domain_service import OrderDomainService
+from order_service.order_domain.application_service.order_create_helper import OrderCreateHelper
+from order_service.order_domain.application_service.ports.output.message.publisher.payment.order_created_payment_request_message_publisher import \
+    OrderCreatedPaymentRequestMessagePublisher
 
 
 class OrderCreateCommandHandler:
     """Handler for processing order creation commands."""
     def __init__(
         self,
-        order_domain_service: OrderDomainService,
-        order_repository: OrderRepository,
-        customer_repository: CustomerRepository,
-        restaurant_repository: RestaurantRepository,
-        order_data_mapper: OrderDataMapper
+        order_create_helper: OrderCreateHelper,
+        order_data_mapper: OrderDataMapper,
+        order_created_payment_request_message_publisher: OrderCreatedPaymentRequestMessagePublisher
     ):
-        self.order_domain_service = order_domain_service
-        self.order_repository = order_repository
-        self.customer_repository = customer_repository
-        self.restaurant_repository = restaurant_repository
+        self.order_create_helper = order_create_helper
         self.order_data_mapper = order_data_mapper
+        self.order_created_payment_request_message_publisher = order_created_payment_request_message_publisher
 
-    # TODO: make this method transactional
     def create_order(self, command: CreateOrderCommand) -> CreateOrderResponse:
-        self.check_customer(command.customer_id)
-        restaurant = self.check_restaurant(command)
-        order = self.order_data_mapper.create_order_command_to_order(command)
-        order_created_event = self.order_domain_service.validate_and_initiate_order(order, restaurant)
-        persisted_order = self.save_order(order)
+        order_created_event = self.order_create_helper.persist_order(command)
+        logging.info(f'Order created with id {order_created_event.order.id}')
+        self.order_created_payment_request_message_publisher.publish(order_created_event)
         return self.order_data_mapper.order_to_create_order_response(
-            persisted_order, 'Order created successfully'
+            order_created_event.order, 'Order created successfully'
         )
 
 
-
-    def check_restaurant(self, command: CreateOrderCommand):
-        restaurant = self.order_data_mapper.create_order_command_to_restaurant(command)
-        optionalRestaurant = self.restaurant_repository.find_restaurant_information(restaurant)
-        if not optionalRestaurant:
-            logging.warning(f'could not find restaurant {command.restaurant_id}')
-            raise OrderDomainException(f'could not find restaurant {command.restaurant_id}')
-        return optionalRestaurant
-
-    def check_customer(self, customer_id: UUID):
-        customer = self.customer_repository.find_customer(customer_id)
-        if not customer:
-            logging.warning(f'could not find customer {customer_id}')
-            raise OrderDomainException(f'could not find customer {customer_id}')
-
-    def save_order(self, order):
-        result = self.order_repository.save(order)
-        if not result:
-            logging.warning('Failed to save order')
-            raise OrderDomainException('Failed to save order')
-        logging.info(f'Saved order with id {order.id}')
-        return result
---
+++ projects/order-service/src/order_service/order_domain/application_service/order_create_helper.py
@@ -0,0 +1,60 @@
+import logging
+
+from order_service.order_domain.application_service.dto.create.create_order_command import CreateOrderCommand
+from order_service.order_domain.application_service.mapper.order_data_mapper import OrderDataMapper
+from order_service.order_domain.application_service.ports.output.repository.customer_repository import \
+    CustomerRepository
+from order_service.order_domain.application_service.ports.output.repository.order_repository import OrderRepository
+from order_service.order_domain.application_service.ports.output.repository.restaurant_repository import \
+    RestaurantRepository
+from order_service.order_domain.domain_core.event.order_created_event import OrderCreatedEvent
+from order_service.order_domain.domain_core.exception.order_domain_exception import OrderDomainException
+from order_service.order_domain.order_domain_service import OrderDomainService
+
+
+class OrderCreateHelper:
+    def __init__(
+        self,
+        order_domain_service: OrderDomainService,
+        order_repository: OrderRepository,
+        customer_repository: CustomerRepository,
+        restaurant_repository: RestaurantRepository,
+        order_data_mapper: OrderDataMapper
+    ):
+        self.order_domain_service = order_domain_service
+        self.order_repository = order_repository
+        self.customer_repository = customer_repository
+        self.restaurant_repository = restaurant_repository
+        self.order_data_mapper = order_data_mapper
+
+    # TODO: make transactional
+    def persist_order(self, command: CreateOrderCommand) -> OrderCreatedEvent:
+        self.check_customer(command.customer_id)
+        restaurant = self.check_restaurant(command)
+        order = self.order_data_mapper.create_order_command_to_order(command)
+        order_created_event = self.order_domain_service.validate_and_initiate_order(order, restaurant)
+        self.save_order(order)
+        logging.info(f'Order created with id {order_created_event.order.id}')
+        return order_created_event
+
+    def check_restaurant(self, command: CreateOrderCommand):
+        restaurant = self.order_data_mapper.create_order_command_to_restaurant(command)
+        optionalRestaurant = self.restaurant_repository.find_restaurant_information(restaurant)
+        if not optionalRestaurant:
+            logging.warning(f'could not find restaurant {command.restaurant_id}')
+            raise OrderDomainException(f'could not find restaurant {command.restaurant_id}')
+        return optionalRestaurant
+
+    def check_customer(self, customer_id: UUID):
+        customer = self.customer_repository.find_customer(customer_id)
+        if not customer:
+            logging.warning(f'could not find customer {customer_id}')
+            raise OrderDomainException(f'could not find customer {customer_id}')
+
+    def save_order(self, order):
+        result = self.order_repository.save(order)
+        if not result:
+            logging.warning('Failed to save order')
+            raise OrderDomainException('Failed to save order')
+        logging.info(f'Saved order with id {order.id}')
+        return result
